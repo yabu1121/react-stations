c27c7daf0319f52c84b9c42cede20a45f35cf894
ここにユーザー名を入力 <>
2024-08-21 03:09:18 +0900

    少し

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 // DO NOT DELETE
 
+import React from 'react';
 import './App.css'
+import { useState } from 'react';
 
 /**
  * @type {() => JSX.Element}
  */
+
 export const App = () => {
+  const [dogUrl,setDogUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg');
   return (
     <div>
-      <header>yabuのアプリ</header>
-      <h2>Hello, world!</h2>
-      <p>犬の画像を表示するサイトですう</p>
+      <header>DOGアプリ</header>
+      <p>犬の画像を表示するサイトです</p>
+      <img src={dogUrl} alt="犬の画像を表示するサイトです。" />
+      <button onClick={()=>{setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg')}}>更新</button>
     </div>
   )
 }
+
+export default App;
