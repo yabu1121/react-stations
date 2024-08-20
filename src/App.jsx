bb509e9a7544c2ac538c55aa6d0861ff58dccba6
ここにユーザー名を入力 <>
2024-08-21 04:15:09 +0900

    415

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,26 @@ import { useState } from 'react';
 
 export const App = () => {
   const [dogUrl,setDogUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg');
+
+  //fetch('url').then(()=>{成功時の処理}).catch(()=>{失敗時の処理})
+  //thenはPromiseと同じ感じ
+  //処理の時の引数をいまいち理解できていない
+  const reload = () =>{
+  fetch('https://dog.ceo/api/breeds/image/random')
+  .then((res)=>{
+    return res.json();
+  })
+  .then((data) => {
+    setDogUrl(data.message);
+  })
+  }
+
   return (
     <div>
       <header>DOGアプリ</header>
       <p>犬の画像を表示するサイトです</p>
       <img src={dogUrl} alt="犬の画像を表示するサイトです。" />
-      <button onClick={()=>{setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg')}}>更新</button>
+      <button onClick={reload}>更新</button>
     </div>
   )
 }
